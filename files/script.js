f79b639262abeada3e34ed082be416dc4e571ba4
SARANYA S <141751770+Saranya-S1608@users.noreply.github.com>
2024-01-13 17:46:18 +0530

    Add files via upload

---
+++ files/script.js
@@ -0,0 +1,19 @@
+
+var typed = new Typed(".text", {
+    strings: ["Programming" , "Web Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
+const toTop = document.querySelector(".top");
+window.addEventListener("scroll",() =>{
+    if (window.pageYOffset > 100){
+        toTop.classList.add("active");
+    }
+    else{
+        toTop.classList.remove("active");
+    }
+})
